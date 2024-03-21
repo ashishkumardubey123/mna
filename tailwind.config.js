7a40f69396fddb077ca8c17ed7bf161a48b6a2e3
deepali verma <deepaliverma064@gamil.com>
2024-03-21 17:27:22 +0530

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
-const {
-  default: flattenColorPalette
-} = require("tailwindcss/lib/util/flattenColorPalette")
+
+const flattenColorPalette =
+  require("tailwindcss/lib/util/flattenColorPalette").default;
+
+const colors = require("tailwindcss/colors");
+// const {
+//   default: flattenColorPalette,
+// } = require("tailwindcss/lib/util/flattenColorPalette");
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -9,38 +25,37 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
+  darkMode: "class",
   theme: {
     extend: {
       animation: {
+        scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
         aurora: "aurora 60s linear infinite",
+        shimmer: "shimmer 2s linear infinite",
       },
       keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
         aurora: {
           from: {
             backgroundPosition: "50% 50%, 50% 50%",
           },
           to: {
             backgroundPosition: "350% 50%, 350% 50%",
+            transform: "translate(calc(-50% - 0.5rem))",
           },
         },
       },
-      // backgroundImage: {
-      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      //   "gradient-conic":
-      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      // },
     },
   },
-  plugins: [addVariablesForColors]
+  plugins: [addVariablesForColors],
 };
 
-function addVariablesForColors({ addBase, theme }) {
-  let allColors = flattenColorPalette(theme("colors"))
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  )
 
-  addBase({
-    ":root": newVars
-  })
-}
