e133c9878b9a5b703a8987b1b003a434d99bff4d
deepali verma <deepaliverma064@gamil.com>
2024-03-22 13:36:00 +0530

    some changes

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,9 +1,12 @@
+
 "use client";
 
 import { motion } from "framer-motion";
 import React from "react";
 import { AuroraBackground } from "./ui/aurora-background";
-import { InfiniteMovingCards } from "./ui/infinite-moving-cards"
+
+
+
 
 
 export function Hero() {
@@ -11,7 +14,7 @@ export function Hero() {
     <>
       <AuroraBackground>
         <motion.div
-          initial={{ opacity: 0.0, y: 40 }}
+          initial={{ opacity: 0, y: 40 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{
             delay: 0.3,
@@ -27,60 +30,36 @@ export function Hero() {
             And this, is chemical burn.
           </div>
           <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[3px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-8 py-5 text-base font-medium text-white backdrop-blur-3xl">
-          Know More
-        </span>
-      </button>
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-8 py-5 text-base font-medium text-white backdrop-blur-3xl">
+              Know More
+            </span>
+          </button>
         </motion.div>
       </AuroraBackground>
 
 
-      <div>
-        {" "}
+
+      {/* <div>
         <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
           <InfiniteMovingCards
-            items={testimonials}
+            cards={Cards.map((card, index) => (
+              <div key={index} className="card">
+                <div>{card.icon}</div>
+                <h1>{card.heading}</h1>
+                <p>{card.discription}</p>
+                <button>View More</button>
+              </div>
+            ))}
             direction="right"
             speed="slow"
           />
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
 
 
 
-const testimonials = [
-  {
-    quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Charles Dickens",
-    title: "A Tale of Two Cities"
-  },
-  {
-    quote:
-      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: "William Shakespeare",
-    title: "Hamlet"
-  },
-  {
-    quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream"
-  },
-  {
-    quote:
-      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
-    title: "Pride and Prejudice"
-  },
-  {
-    quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick"
-  }
-]
 export default Hero;
